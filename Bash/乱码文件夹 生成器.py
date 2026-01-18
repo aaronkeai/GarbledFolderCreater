d9d7ef780ea956cc55856cbf7831312717e71fd9
Chung Chai Aaron Dong <aaron.dongsq@hotmail.com>
2026-01-18 14:00:12 +0800

    上传源代码

---
+++ Bash/乱码文件夹 生成器.py
@@ -0,0 +1,135 @@
+﻿from os import makedirs
+from os.path import join, exists
+from random import randint, choice
+from time import sleep
+
+class CreateFolder:
+    def __init__(self, link:str, number:str):
+        '''
+        没错！这个类负责创建文件夹，是后端！
+        跟我前面的兄弟不一样，我负责与系统交互！
+        呃，当然，如果我在创建文件夹出错了，我会甩出报错，提示用户
+        不过，既然要让我创建文件夹，你要给我路径和创建数量，我才给你创建
+
+        下面__init__函数说明：
+        负责创建文件夹这个核心工作，至于乱码生成和其他？这个是其他函数干的事情
+        '''
+        try:
+            for i in range(int(number)):
+                makedirs(join(link, self._set_folder_name())) #创建文件夹
+            print('创建成功了！')
+        except KeyboardInterrupt:
+            '''
+            说明：由于创建文件夹可能会有一些不可控因素（例子：系统大粪、用户疏忽、权限不足等）
+            try会捕获全部异常，包括KeyboardInterrupt。全局的肯定失效了
+            所以需要这样捕获，然后再次甩出KeyboardInterrupt，让全局能够捕获这个异常，从而提示用户已退出。
+            '''
+            raise KeyboardInterrupt('用户取消了文件夹创建。')
+        except FileExistsError: #能触发这个报错的，你运气是真的好
+            print('''
+【软件惊叹】老天爷！你这运气去买彩票吧！
+软件万万没想到随机数竟然能撞车创建出同名文件夹，你居然中奖了！
+如果可以的话，建议你今天出门左转买一张彩票，如果这个软件对你有帮助，中了记得分我一些。
+
+不过如果你仍想要创建文件夹，请重启该软件。''')
+        except Exception as e: #其他问题报错汇总
+            print('''【软件异常】创建文件夹时发生了错误...
+通常，文件夹创建出错是由下面的原因导致的：
+1)  你输入了一个存在的目录，然后你把它删了。
+2)  你没有授予该软件相应的权限去创建相应权限的文件夹。
+3)  你想创建的目录所在的磁盘空间已满。
+4） 创建文件夹中途你的磁盘坏了。
+
+如果你已排查上述原因，但是还是异常，请联系作者解决。
+报错提示：'''+str(e)) #友善化的报错排查提示
+
+    def _choose_letter(self, how_long:int) -> str:
+        '''
+        我，负责随机生成一段乱文，也是乱码文件夹生成器的一个重要零件。
+        不过开始之前，你先给我输入一个数字，决定我到底要生成多长的乱文
+        我生成好了后会给你
+        '''
+        letter = 'abcdefghijklmnopqrstuvwxyz1234567890'
+        return ''.join(choice(letter) for _ in range(how_long)) #返回已经搞定的乱文
+    
+    def _set_folder_name(self) -> str:
+        '''
+        负责乱码文件夹的命名。
+        不需要向该函数提供任何名称，可以直接调用。
+        当工作完成时，返回乱码文件夹的值。
+        '''
+        template = ['%s-%s-%s-%s','%s-%s-%s',
+                    '%s-%s','%s'] #乱码文件夹名称的名称模板
+        choosed_template = choice(template) #选择一个模板
+        word_temp = [] #乱文的暂存列表
+        for i in range(choosed_template.count('%s')):
+            word_temp.append(self._choose_letter(randint(5, 10)))
+        #下方的return将会现场组合好乱码文件夹的名称，然后返回
+        return choosed_template%tuple(word_temp)
+
+
+class Main:
+    def __init__(self):
+        '''
+    这是软件的前端，负责收集必要的信息
+    你问创建文件夹？那请你去CreateFolder这个类，你找错人了
+    啊不对，我是个类，咳咳，你找错类了，请你找我上面的兄弟
+
+    下面的__init__函数说明：
+    这是整个软件的向导，负责询问用户问题。
+        '''
+        while True: 
+            '''
+            循环·第一关
+            内容：问用户要把文件夹创建在哪里
+            如果用户输入正确目录（或者盘符），循环打断
+            否则，一直循环。
+            '''
+            where_to_storage = input('你想在哪里创建文件夹？\n创建位置：') #向用户询问在哪里创建
+            if self._check_folder_exists(where_to_storage):
+                #若文件夹存在（或者用户输入盘符），则打破循环
+                break
+            print('请输入一个存在的路径！\n')
+
+        while True:
+            '''
+            循环·第二关
+            内容：问用户要创建多少个文件夹
+            与第一关相似，但是会问用户希望创建多少个文件夹
+            如果用户输入正确的阿拉伯数字，这关过，否则一直循环
+            '''
+            folder_number = input('\n你要创建多少个文件夹？\n创建数量：') #向用户询问创建多少文件夹
+            if self._check_number_exists(folder_number):
+                #数字合规方可过此关
+                break
+            #到这里，你通过了几关呢？
+        print('\n创建中，请稍候...')
+        CreateFolder(where_to_storage, folder_number)
+
+    def _check_folder_exists(self, link:str) -> bool:
+        '''
+        负责检查文件夹地址是否存在。
+        如果文件夹地址是存在的，返回True，反之False。
+        '''
+        return exists(link)
+
+    def _check_number_exists(self, number:str) -> bool:
+        '''
+        负责检查数字输入是否有效，如果有效则返回Yes。
+        如果无效，甚至报错的，会提示（承担了一部分向导工作），并返回False。
+        '''
+        try:
+            if int(number) <= 0: #是否创建小于等于0个文件夹
+                print('不可以创建小于或等于0个文件夹！')
+                return False
+            else: #如果不是
+                return True
+        except ValueError: #用户输入非阿拉伯数字
+            print('请输入阿拉伯整数，而不是其他内容！')
+            return False
+
+
+try: #全局捕捉KeyboardInterrupt错误
+    Main()
+except KeyboardInterrupt:
+    print('\n软件已退出。')
---
+++ GUI/乱码文件夹-生成器.py
@@ -0,0 +1,212 @@
+﻿import tkinter as tk
+import tkinter.ttk as ttk
+import tkinter.filedialog as fileask
+
+import sys
+from random import randint, choice
+from os.path import join, exists
+
+class CreateFolder:
+    def __init__(self, main:tk.Tk, path:str, number:str):
+        '''
+        这个类负责创建文件夹。
+        main：主窗口
+        path：用户选择的路径
+        number：用户希望生成的文件夹。
+        
+        __init__说明：
+        这个函数仅负责创建这个核心操作。
+        涉及其他方面（如文件夹存在检查、数字合法检查等），将会交给其他函数处理。
+        '''
+        main.tiplabel.place_forget() #清空状态
+        foldercheck_result = self._check_folder_exists(main, path)
+        numbercheck_result = self._check_number_exists(main, number)
+
+    def _check_folder_exists(self, main:tk.Tk, path:str) -> bool:
+        '''
+        检查用户输入的文件夹路径是否存在。
+        如果不存在，那么将会提示用户，返回False。
+        如果存在，那么放行，返回True。
+        '''
+        if not exists(path): #检查文件夹是否存在
+            main.tiplabel.config(text='必须指定一个存在的文件夹。')
+            main.tiplabel.place(x=20, y=200, width=510, height=30) #提示用户
+            return False
+        else:
+            return True
+    
+    def _check_number_exists(self, main:tk.Tk, number:str) -> bool:
+        '''
+        检查用户输入的数字是否合规的阿拉伯整数。
+        如果不合规甚至报错的，那么提示用户，返回False
+        如果合规，那么放行，返回True。
+        '''
+        try:
+            if int(number) <= 0: #判断输入的数是否小于等于0
+                main.tiplabel.config(text='生成的文件夹不能小于等于0。')
+                main.tiplabel.place(x=20, y=200, width=510, height=30) 
+            else:
+                return True
+        except ValueError: #在int过程中如果出错那么不合规
+            main.tiplabel.config(text='必须输入阿拉伯整数。')
+            main.tiplabel.place(x=20, y=200, width=510, height=30) 
+
+class MainWindow(tk.Tk):
+    #====================窗口&控件部分====================
+    def __init__(self):
+        '''
+        这里会初始化窗口的设置，至于控件？会交给set_components负责
+        主菜单的配置会在这里配置，但是子菜单和孙菜单会在_child_menu_create配置
+        '''
+        super().__init__()
+        self.title('乱码文件夹 生成器')
+        self.winfo_geometry(self, 550, 380)
+        self.attributes('-alpha', 0.8)
+        self.protocol('WM_DELETE_WINDOW', sys.exit)
+        
+        self._set_components() #开始创建控件
+
+        #-----菜单：创建----
+        self.menu = tk.Menu() 
+        self.filemenu = tk.Menu(tearoff=0)
+        self.gerenatemenu = tk.Menu(tearoff=0)
+        self.aboutmenu = tk.Menu(tearoff=0)
+        
+        #-----子菜单：创建----
+        self.filemenu.alpha = tk.Menu(tearoff=0)
+
+        self._child_menu_create() #拼接，启动！
+
+        #-----主菜单：菜单项目部分&设置窗口-----
+        self.menu.add_cascade(label='文件', menu=self.filemenu)
+        self.menu.add_cascade(label='生成', menu=self.gerenatemenu)
+        self.menu.add_cascade(label='关于', menu=self.aboutmenu)
+
+        self.config(menu=self.menu)
+
+    def _child_menu_create(self):
+        '''
+        负责组装菜单，给宝贵的__init__函数留点空间
+        '''
+
+        #-----filemenu子菜单：透明度调节-----
+        alpha_options = [
+            ('0.2 （不建议）', 0.2),
+            ('0.4', 0.4),
+            ('0.6', 0.6),
+            ('0.8 （默认）', 0.8),
+            ('不透明', 1.0)
+        ] #透明度选项：（标注文本，输入的值）
+        for label, opinions in alpha_options:
+            self.filemenu.alpha.add_command(label=label, 
+                 command=lambda v=opinions: self.attributes('-alpha', v))
+
+        #-----filemenu菜单-----
+        self.filemenu.add_cascade(label='窗口透明度', menu=self.filemenu.alpha)
+        self.filemenu.add_separator()
+        self.filemenu.add_command(label='退出软件（确定吗？）', foreground='red', 
+                                  activebackground='#8f3636', 
+                                  activeforeground='white', command=sys.exit)
+        #-----generatemenu菜单---
+        generate_options = [10, 20, 30, 50, 
+                            100, 200, 300, 500, 
+                            1000, 5000, 10000] #生成多少个文件夹，数字指要生成多少个
+        for number in generate_options:
+            self.gerenatemenu.add_command(label=f'生成{str(number)}个乱码文件夹...',
+                                          command=lambda num=number: self.confirmed_numfolder_(num))
+
+        #-----aboutmenu菜单-----
+        self.aboutmenu.add_command(label='关于该软件')
+        self.aboutmenu.add_command(label='开源许可证...')
+        self.aboutmenu.add_separator()
+        self.aboutmenu.add_command(label='支持该软件')
+
+    def _set_components(self):
+        '''
+        负责创建窗口内的控件。
+        光有窗口没啥用，如果只有窗口的软件那叫做空壳子，而不是GUI。
+        要变成GUI，先要窗口，其次控件。
+        '''
+        ##########Labelframe内控件##########
+        #---frame初始化&文本设置---
+        framesettings = [('生成位置：', 20, 10),
+                         ('生成数量：', 20, 45),
+                         ('个', 430, 45)]#设置文本输入参数：(文本, X, Y)
+        self.labelframe = ttk.Labelframe(self, text='生成设置', width=490, height=160)
+        self.labelframe.place(x=30, y=20)
+        for text,x,y in framesettings: #设置文本（针对Labelframe内）
+            tk.Label(self.labelframe, text=text, font=('', 10, 'bold')).place(x=x, y=y)
+
+        #---输入框部分---
+        self.putwhere_entry = ttk.Entry(self.labelframe, font=('楷体', 12))
+        self.putwhere_entry.place(x=115, y=9, width=340, height=25) #生成位置 输入框
+        self.howmany_entry = ttk.Entry(self.labelframe, font=('楷体', 12))
+        self.howmany_entry.place(x=115, y=45, width=310, height=25) #生成数量 输入框
+        
+        #---按钮部分---
+        self.btn_storage = tk.Button(self.labelframe, text='让我选择存放位置！' ,
+         bg='green', fg='white', border=0, relief='flat', font=('', 10, 'bold'),
+        activebackground='#FCB827', activeforeground='white', 
+        command=self.askwheretostroge_)
+        self.btn_storage.place(x=25, y=83, width=200, height=33) #选择存放位置 按钮
+        self.btn_ramdom = tk.Button(self.labelframe, text='文件夹数量随机' ,
+        bg='#9200d0', fg='white', border=0,  relief='flat', font=('', 10, 'bold'),
+        activebackground='#FCB827', activeforeground='white',
+        command=self.random_numfolder_)
+        self.btn_ramdom.place(x=260, y=83, width=200, height=33) #文件夹数量随机 按钮
+
+        ##########self直接创建##########
+        #-----提示文本-----
+        self.tiplabel = tk.Label(self, text='测试内容，此文本不该在正常运作时出现',
+                                 fg='red' , font=('楷体', 14, 'bold')) #这个控件不显示，在用户做出错误操作时才会显示
+
+        #-----按钮部分-----
+        self.actionbutton = tk.Button(self, text='开始生成！', bg='green', fg='white',
+                                      relief='flat', border=0, width=20, height=2,
+                                      font=('', 10, 'bold'), 
+                                      activebackground='#FCB827', activeforeground='white',
+                                      command=lambda: CreateFolder(self, self.putwhere_entry.get(), self.howmany_entry.get())) #开始按钮生成
+        self.actionbutton.place(x=175, y=250)
+    #====================窗口控件交互部分====================
+    def askwheretostroge_(self):
+        '''
+        负责询问用户需要把乱码文件夹存储到哪里。
+        如果用户选择好了，那么将会修改生成位置输入框内的东西，变成用户选择好的目录。
+        与按钮“让我选择存放位置！”联系。
+        '''
+        path = fileask.askdirectory(title='请选择保存的位置')
+        if len(path) > 0: #检查用户到底选择了没有
+            self.putwhere_entry.delete('0','end')
+            self.putwhere_entry.insert('0', path)
+
+    def random_numfolder_(self):
+        '''
+        这个函数将会随机抽取一个数，然后修改生成数量输入框内的东西。
+        与按钮“文件夹数量随机”有联系。
+        '''
+        self.howmany_entry.delete('0','end')
+        #↓ 随机抽取100~1000的数
+        self.howmany_entry.insert('0',randint(100, 1000))
+
+    def confirmed_numfolder_(self, number:int):
+        '''
+        这个函数将会根据填入的数值（取决于number填的是啥），修改生成数量文件夹的输入框。
+        与菜单中generatemenu中“生成XXX个文件夹...”有关系。
+        '''
+        self.howmany_entry.delete('0', 'end')
+        self.howmany_entry.insert('0', str(number))
+    #====================对外开放控件部分====================
+    def winfo_geometry(self, master:tk.Tk, x:int, y:int):
+        '''
+        如果使用该函数，在窗口设置大小的时候，默认居中在窗口中央。
+        这个函数的代码在其他函数是通用的
+        （Tips：默认设置最小窗口伸展为你输入的数值）
+        '''
+        screenwidth = (master.winfo_screenwidth() - x)/2
+        screenheight = (master.winfo_screenheight() - y)/2
+        master.geometry(f'{x}x{y}+{int(screenwidth)}+{int(screenheight)}')
+        master.minsize(x, y)
+
+if __name__ == '__main__':
+    app = MainWindow()
+    app.mainloop()
